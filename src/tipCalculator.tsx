ecc4b14be5879a6fd16480781333661f642c028f
Michael Olmstead <molmstead4876@xmail.dixietech.edu>
2024-07-25 08:36:47 -0700

    Modified css and added barebones component

---
+++ src/tipCalculator.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useState } from "react";
+
+const tipCalculator: React.FC = () => {
+  const [bill, setBill] = useState("");
+  const [tip, setTip] = useState("");
+
+  const [totalBill, setTotalBill] = useState<number>();
+};
