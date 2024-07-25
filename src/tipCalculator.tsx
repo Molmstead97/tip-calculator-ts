97493ed4b9aac283dcdc97faebcf70a0633435c6
Michael Olmstead <molmstead4876@xmail.dixietech.edu>
2024-07-25 09:31:17 -0700

    Added the event handle and calculations

--- src/tipCalculator.tsx
+++ src/tipCalculator.tsx
@@ -1,17 +1,44 @@
 import React from "react";
 import { useState } from "react";
 
+// Conforms the inputs to a specific structure
 interface FormInput {
-  bill: number | string;
-  tip: number | string;
+  bill: string;
+  tip: string;
 }
 
 const tipCalculator: React.FC = () => {
   const [formInput, setFormInput] = useState<FormInput>({ bill: "", tip: "" });
 
-  const [totalBill, setTotalBill] = useState<number | null>(null);
+  const [totalBill, setTotalBill] = useState<string | null>(null);
 
-  return (
+  
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     
+    // 'Name' refers to bill/tip element depending on which triggered the event. Value is the current value of the element 
+    const { name, value } = event.target;
+    
+    // This spreads everything into the new object and updates it (eg. bill/tip)
+    setFormInput(prevInput => ({
+      ...prevInput,
+      [name]: value,
+    }));
+  };
+
+  const calculateTip = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    
+    const bill = parseFloat(formInput.bill)
+    const tip = parseFloat(formInput.tip)
+
+    const tipAmount = bill * (tip / 100)
+    const total = bill + tipAmount
+    setTotalBill(`Your total is $${total.toFixed(2)}`)
+
+
+  }
+  
+  return (
+
   )
 };
