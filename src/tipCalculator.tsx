26a0852ad58914b2b80e827e0d13e9b98c2f295f
Michael Olmstead <molmstead4876@xmail.dixietech.edu>
2024-07-25 08:44:47 -0700

    Modified to use an interface

--- src/tipCalculator.tsx
+++ src/tipCalculator.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { useState } from "react";
 
-const tipCalculator: React.FC = () => {
-  const [bill, setBill] = useState("");
-  const [tip, setTip] = useState("");
+interface FormInput {
+  bill: number | string;
+  tip: number | string;
+}
 
-  const [totalBill, setTotalBill] = useState<number>();
+const tipCalculator: React.FC = () => {
+  const [formInput, setFormInput] = useState<FormInput>({ bill: "", tip: "" });
+
+  const [totalBill, setTotalBill] = useState<number | null>(null);
+
+  return (
+    
+  )
 };
